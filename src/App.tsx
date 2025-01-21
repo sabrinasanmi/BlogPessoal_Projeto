5215b42812b77cbf92379351c6b6446be58ce222
sabrinasanmi <sanmisabrina@icloud.com>
2025-01-20 21:00:28 -0300

    Criar componente Cadastro.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
-import './App.css'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
   return (
     <>
         <BrowserRouter>
           <Navbar />
-          <div className="min-h-[80vh]">
+          <div className="min-h-80vh]">
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
+              {/* <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} /> */}
+              <Route path="/" element={<Cadastro />} />
             </Routes>
           </div>
           <Footer />
